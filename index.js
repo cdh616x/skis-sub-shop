c61f6c862e699a4dc61f870d9dcc89bc46699cf6
cdh666x <91150236+cdh666x@users.noreply.github.com>
2021-11-09 15:46:22 -0500

    figuring out the sign functioning

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function getTime() {
   var day = new Date();
-  var day = day.getDay();
-  if (day === 6 || 0 || 2) {
+  var whatDay = day.getDay();
+  var hour = day.getHours();
+  var minute = day.getMinutes();
+
+  if (whatDay === 6 || 0) {
     $("#sign").attr("src", "images/neon-closed.jpeg");
+  } else if (hour < 10 && minute >= 30 || hour >= 15) {
+    $("#sign").attr("src", "images/neon-closed.jpeg");
+  } else {
+    $("#sign").attr("src", "images/open-neon.jpeg");
   }
+  console.log(hour);
   console.log(day);//-------works; ouputs Sunday as 0 & Saturday as 6
 }
 
