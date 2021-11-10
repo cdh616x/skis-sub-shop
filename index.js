6a5db6567ccbf444bdc33e90db8380f888abbe46
cdh666x <91150236+cdh666x@users.noreply.github.com>
2021-11-10 09:02:08 -0500

    changed buggy js

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function getTime() {
 
   if (whatDay === 6 || 0) {//---------closed sign on Saturday and Sunday
     $("#sign").attr("src", "images/neon-closed.jpeg");
-  } else if (hour <= 10 && minute < 30 || hour >= 15) {//----------closed sign between the hours of 3pm and 10:30am
+  } else if (hour < 10 || hour >= 15) {//----------closed sign between the hours of 3pm and 10am
     $("#sign").attr("src", "images/neon-closed.jpeg");
+  } else if (hour === 10 && minute < 30) {//-----------closed between 10 and 10:30
+    $("#sign").arrt("src", "images/neon-closed.jpeg");
   } else {
-    $("#sign").attr("src", "images/open-neon.jpeg");
+    $("#sign").attr("src", "images/neon-open.jpeg");
   }
   console.log(hour);
   console.log(day);//-------works; ouputs Sunday as 0 & Saturday as 6
