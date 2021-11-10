e9b235935e4c1395932a90900b19141ac9fb37b3
cdh666x <91150236+cdh666x@users.noreply.github.com>
2021-11-10 12:16:38 -0500

    put phone number in banner and added js logic to it to change text based on time

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getTime() {
   } else if (hour <= 10 || hour >= 15) {//----------closed sign between the hours of 3pm and 10am
     if (hour === 10 && minute < 30) {
       $("#sign").attr("src", "images/neon-closed.jpeg");
-    }    
+    }
   } else {
     $("#sign").attr("src", "images/neon-open.jpeg");
   }
@@ -17,7 +17,27 @@ function getTime() {
   console.log(day);//-------works; ouputs Sunday as 0 & Saturday as 6
 }
 
+function openClose() {
+  var day = new Date();
+  var whatDay = day.getDay();
+  var hour = day.getHours();
+  var minute = day.getMinutes();
+
+  if (whatDay === 6 || 0) {//---------closed sign on Saturday and Sunday
+    $(".open-close").text("Sorry, we're closed!");
+  } else if (hour <= 10 || hour >= 15) {//----------closed sign between the hours of 3pm and 10am
+    if (hour === 10 && minute < 30) {
+      $(".open-close").text("Sorry, we're closed!");
+    }
+  } else {
+    $(".open-close").text("We're open; Call now to order!");
+  }
+  console.log(hour);
+  console.log(day);//-------works; ouputs Sunday as 0 & Saturday as 6
+}
+
 getTime();
+openClose();
 
 function signFlicker() {
   $("#sign").animate({opacity: 0.2}, 400).animate({opacity: 1}, 200).animate({opacity: 0.3}, 400).animate({opacity: 1}, 200).animate({opacity: 0.4}, 400).animate({opacity: 1}, 200).animate({opacity: 0.5}, 400).animate({opacity: 1}, 200).animate({opacity: 0.6}, 400).animate({opacity: 1}, 200).animate({opacity: 0.7}, 400).animate({opacity: 1}, 200);
